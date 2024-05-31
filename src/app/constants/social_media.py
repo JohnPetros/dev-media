78604ef091445169893ac292921754ddd78b4ec0
JohnPetros <joaopedro.nc@outlook.com>
2024-05-31 13:52:59 -0300

    🪨 constants: add social media

---
+++ src/app/constants/social_media.py
@@ -0,0 +1,18 @@
+SOCIAL_MEDIA = {
+    "github": {
+        "api_url": "https://api.github.com/users",
+        "url": "https://github.com",
+    },
+    "twitter": {
+        "api_url": "https://ded640a2-f8a1-48cd-980b-58e153f6203c-00-3lzx8qbm17350.riker.replit.dev/user",
+        "url": "https://x.com",
+    },
+    "instagram": {
+        "api_url": "https://i.instagram.com/api/v1/users/web_profile_info/?username=",
+        "url": "https://www.instagram.com/",
+    },
+    "youtube": {
+        "api_url": "https://www.googleapis.com/youtube/v3",
+        "url": "https://www.youtube.com",
+    },
+}
