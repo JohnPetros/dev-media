82d12fb47f4b2528ed7dc3e463e97f28dacfe694
JohnPetros <joaopedro.nc@outlook.com>
2024-05-31 13:53:31 -0300

    ➕ providers: add twitter

---
+++ src/app/providers/twitter_provider.py
@@ -0,0 +1,34 @@
+from requests import get
+
+from constants import SOCIAL_MEDIA
+
+
+class TwitterProvider:
+    def __init__(self, username: str):
+        self.api_url = SOCIAL_MEDIA["twitter"]["api_url"]
+        self.url = SOCIAL_MEDIA["twitter"]["url"]
+        self.username = username
+        self.data = None
+
+        try:
+            response = get(f"{self.api_url}/{self.username}")
+            data = response.json()
+            self.data = data
+
+        except Exception as exeption:
+            print("Twitter API Error: ", exeption)
+
+    def get_followers_count(self):
+        if self.data is None:
+            return "Indisponível"
+
+        return self.data["followersCount"]
+
+    def get_likes_count(self):
+        if self.data is None:
+            return "Indisponível"
+
+        return self.data["likeCount"]
+
+    def get_link(self):
+        return f"{self.url}/{self.username}"
