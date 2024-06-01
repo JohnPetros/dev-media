c246104ac158d4587dabee5c53974cbbaed09b09
JohnPetros <joaopedro.nc@outlook.com>
2024-06-01 11:39:49 -0300

    🖥️ views: pass social media icons color to home page

--- src/app/infra/views/home_page_view.py
+++ src/app/infra/views/home_page_view.py
@@ -15,20 +15,20 @@ def home_page_view():
         developer = data["developer"]
         social_media_record = data["social_media_record"]
 
-        print(social_media_record.github_followers_count, flush=True)
-
         return render_template(
             "pages/home.html",
             developer=developer,
             social_media_record=social_media_record,
             github_logo_color=SOCIAL_MEDIA["github"]["color"],
-            github_logo_background_color=SOCIAL_MEDIA["github"]["color"],
+            github_logo_background_color=SOCIAL_MEDIA["github"]["background_color"],
             twitter_logo_color=SOCIAL_MEDIA["twitter"]["color"],
-            twitter_logo_background_color=SOCIAL_MEDIA["twitter"]["color"],
+            twitter_logo_background_color=SOCIAL_MEDIA["twitter"]["background_color"],
             instagram_logo_color=SOCIAL_MEDIA["instagram"]["color"],
-            instagram_logo_background_color=SOCIAL_MEDIA["instagram"]["color"],
+            instagram_logo_background_color=SOCIAL_MEDIA["instagram"][
+                "background_color"
+            ],
             youtube_logo_color=SOCIAL_MEDIA["youtube"]["color"],
-            youtube_logo_background_color=SOCIAL_MEDIA["youtube"]["color"],
+            youtube_logo_background_color=SOCIAL_MEDIA["youtube"]["background_color"],
         )
 
     except Exception as exception:
