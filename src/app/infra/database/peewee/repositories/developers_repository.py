595431bce66a1dcf82592652e6962a9ecf83894e
JohnPetros <joaopedro.nc@outlook.com>
2024-06-01 10:03:49 -0300

    🗄️ repositories: get developer by id

--- src/app/infra/database/peewee/repositories/developers_repository.py
+++ src/app/infra/database/peewee/repositories/developers_repository.py
@@ -1,6 +1,23 @@
+from core.entities import Developer
+
 from ..models import DeveloperModel
 
 
 class DevelopersRepository:
     def get_by_id(self, id: int):
-        return DeveloperModel.select(id)
+        try:
+            row = DeveloperModel.get(id)
+
+            developer = Developer(
+                id=row.id,
+                name=row.name,
+                github_username=row.github_username,
+                twitter_username=row.twitter_username,
+                instagram_username=row.instagram_username,
+                youtube_channel=row.youtube_channel,
+                avatar_url=row.avatar_url,
+            )
+
+            return developer
+        except Exception:
+            return None
