ced5ff2611468fa7af86bc4d72dbe792f7526e9a
JohnPetros <joaopedro.nc@outlook.com>
2024-06-01 16:49:43 -0300

    🖥️ views: pass absolute variations to details page

--- src/app/infra/views/details_page_view.py
+++ src/app/infra/views/details_page_view.py
@@ -2,9 +2,8 @@ from os import getenv
 
 from flask import render_template
 
-from core.use_cases import get_page_data
+from core.use_cases import get_page_data, get_details_page_data
 from infra.constants import SOCIAL_MEDIA, INTERNAL_ERROR_MESSAGE
-
 
 developer_id = getenv("DEVELOPER_ID")
 
@@ -16,10 +15,19 @@ def details_page_view():
         developer = data["developer"]
         social_media_record = data["social_media_record"]
 
+        details_page_data = get_details_page_data.execute(social_media_record)
+
+        absolute_variations = details_page_data["absolute_variations"]
+        percentage_variations = details_page_data["percentage_variations"]
+
+        print(percentage_variations)
+
         return render_template(
             "pages/details.html",
             developer=developer,
             social_media_record=social_media_record,
+            absolute_variations=absolute_variations,
+            percentage_variations=percentage_variations,
             github_logo_color=SOCIAL_MEDIA["github"]["color"],
             twitter_logo_color=SOCIAL_MEDIA["twitter"]["color"],
             instagram_logo_color=SOCIAL_MEDIA["instagram"]["color"],
