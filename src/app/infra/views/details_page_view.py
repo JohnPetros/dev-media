6fd382603258a629f34295779626297f1a270ff9
JohnPetros <joaopedro.nc@outlook.com>
2024-06-01 12:28:52 -0300

    🖥️ views: pass social_media_record to details page

--- src/app/infra/views/details_page_view.py
+++ src/app/infra/views/details_page_view.py
@@ -2,47 +2,30 @@ from os import getenv
 
 from flask import render_template
 
-from core.use_cases import get_details_page_data
-from infra.constants import SOCIAL_MEDIA
+from core.use_cases import get_page_data
+from infra.constants import SOCIAL_MEDIA, INTERNAL_ERROR_MESSAGE
 
 
 developer_id = getenv("DEVELOPER_ID")
 
 
 def details_page_view():
-    data = get_details_page_data.execute(developer_id)
+    try:
+        data = get_page_data.execute(developer_id)
 
-    developer = data["developer"]
+        developer = data["developer"]
+        social_media_record = data["social_media_record"]
 
-    github_followers_count = data["github_followers_count"]
-    github_repos_count = data["github_repos_count"]
-    github_stars_count = data["github_stars_count"]
+        return render_template(
+            "pages/details.html",
+            developer=developer,
+            social_media_record=social_media_record,
+            github_logo_color=SOCIAL_MEDIA["github"]["color"],
+            twitter_logo_color=SOCIAL_MEDIA["twitter"]["color"],
+            instagram_logo_color=SOCIAL_MEDIA["instagram"]["color"],
+            youtube_logo_color=SOCIAL_MEDIA["youtube"]["color"],
+        )
 
-    twitter_followers_count = data["twitter_followers_count"]
-    twitter_likes_count = data["twitter_likes_count"]
-    twitter_retweets_count = data["twitter_retweets_count"]
-
-    instagram_followers_count = data["instagram_followers_count"]
-    instagram_posts_count = data["instagram_posts_count"]
-    instagram_likes_count = data["instagram_likes_count"]
-
-    youtube_followers_count = data["youtube_followers_count"]
-
-    return render_template(
-        "pages/details.html",
-        developer=developer,
-        github_followers_count=github_followers_count,
-        github_repos_count=github_repos_count,
-        github_stars_count=github_stars_count,
-        twitter_followers_count=twitter_followers_count,
-        twitter_likes_count=twitter_likes_count,
-        twitter_retweets_count=twitter_retweets_count,
-        instagram_followers_count=instagram_followers_count,
-        instagram_posts_count=instagram_posts_count,
-        instagram_likes_count=instagram_likes_count,
-        youtube_followers_count=youtube_followers_count,
-        github_logo_color=SOCIAL_MEDIA["github"]["color"],
-        twitter_logo_color=SOCIAL_MEDIA["twitter"]["color"],
-        instagram_logo_color=SOCIAL_MEDIA["instagram"]["color"],
-        youtube_logo_color=SOCIAL_MEDIA["youtube"]["color"],
-    )
+    except Exception as exception:
+        print(f"Details Page View Error: {exception}", flush=True)
+        return INTERNAL_ERROR_MESSAGE
