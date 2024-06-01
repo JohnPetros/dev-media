547fdc7b3afc21e545f5a5b9123fbc4bba097eb0
JohnPetros <joaopedro.nc@outlook.com>
2024-06-01 16:50:01 -0300

    🪨 constants: add variations

---
+++ src/app/core/constants/variations.py
@@ -0,0 +1,18 @@
+VARIATIONS = {
+    "absolute_variation_attributes": [
+        "github_followers_count",
+        "twitter_followers_count",
+        "instagram_followers_count",
+        "youtube_subscribers_count",
+    ],
+    "percentage_variation_attributes": [
+        "github_repos_count",
+        "github_stars_count",
+        "twitter_retweets_count",
+        "twitter_likes_count",
+        "instagram_posts_count",
+        "instagram_likes_count",
+        "youtube_views_count",
+        "youtube_videos_count",
+    ],
+}
