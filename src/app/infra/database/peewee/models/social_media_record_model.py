dfe700de664792a693d22e285630db665840897e
JohnPetros <joaopedro.nc@outlook.com>
2024-06-01 10:03:29 -0300

    🗂️ model: add created_at field to social media record model

--- src/app/infra/database/peewee/models/social_media_record_model.py
+++ src/app/infra/database/peewee/models/social_media_record_model.py
@@ -1,4 +1,6 @@
-from peewee import DecimalField, ForeignKeyField
+from datetime import datetime, timezone
+
+from peewee import CharField, ForeignKeyField, DateTimeField
 
 from core.constants import DEFAULT_COUNT_VALUE
 
@@ -9,19 +11,24 @@ from .developer_model import DeveloperModel
 class SocialMediaRecordModel(Model):
     developer = ForeignKeyField(DeveloperModel, backref="social_media_records")
 
-    github_followers_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    twiter_followers_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    instagram_followers_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    youtube_subscribers_count = DecimalField(default=DEFAULT_COUNT_VALUE)
+    github_followers_count = CharField(default=DEFAULT_COUNT_VALUE)
+    twitter_followers_count = CharField(default=DEFAULT_COUNT_VALUE)
+    instagram_followers_count = CharField(default=DEFAULT_COUNT_VALUE)
+    youtube_subscribers_count = CharField(default=DEFAULT_COUNT_VALUE)
 
-    github_repos_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    github_stars_count = DecimalField(default=DEFAULT_COUNT_VALUE)
+    github_repos_count = CharField(default=DEFAULT_COUNT_VALUE)
+    github_stars_count = CharField(default=DEFAULT_COUNT_VALUE)
 
-    instagram_posts_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    instagram_likes_count = DecimalField(default=DEFAULT_COUNT_VALUE)
+    instagram_posts_count = CharField(default=DEFAULT_COUNT_VALUE)
+    instagram_likes_count = CharField(default=DEFAULT_COUNT_VALUE)
 
-    twitter_retweets_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    twitter_likes_count = DecimalField(default=DEFAULT_COUNT_VALUE)
+    twitter_retweets_count = CharField(default=DEFAULT_COUNT_VALUE)
+    twitter_likes_count = CharField(default=DEFAULT_COUNT_VALUE)
 
-    youtube_likes_count = DecimalField(default=DEFAULT_COUNT_VALUE)
-    youtube_videos_count = DecimalField(default=DEFAULT_COUNT_VALUE)
+    youtube_views_count = CharField(default=DEFAULT_COUNT_VALUE)
+    youtube_videos_count = CharField(default=DEFAULT_COUNT_VALUE)
+
+    created_at = DateTimeField(default=datetime.now(timezone.utc))
+
+    class Meta:
+        table_name = "social_media_records"
